5b0c9cf1717bc22ece67419c08aefef7a9787f77
dini28 <dipeshsonitech@gmail.com>
2026-07-26 00:00:19 +0530

    fix: export platform and KYC types from central types entrypoint

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,4 +11,6 @@ export type {
   MessageThreadKind,
   MessageThreadStatus,
 } from "./message";
+export type { PlatformSettings, PlatformAnalytics } from "./platform";
+export type { KycStatus, DealerKycRecord, DealerKycDocument, KycDocumentType } from "./kyc";
 export type { Database, ProfileRow, AppRole, ProfileStatus } from "./database";
